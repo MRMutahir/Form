c0aace89a4cc1cbb0c848d122928feec1ec4fab0
Mhummad Mutahir <mutahirkareem820@gmail.com>
2023-06-22 02:28:08 +0500

    ui

--- Dashbord/dasbord.js
+++ Dashbord/dasbord.js
@@ -3,22 +3,41 @@ let modifyData = JSON.parse(privousData)
 let fullName = document.getElementById('fullName');
 let email = document.getElementById('email');
 let birthday = document.getElementById('birthday');
-console.log(privousData, '==>>privousData');
-console.log(modifyData, '=>>>modifyData')
-console.log(modifyData.firstname, '=>>>');
+let birth1 = document.querySelector('.birth1');
+let birth2 = document.querySelector('.birth2');
+let birth3 = document.querySelector('.birth3');
+let post = document.getElementById('post');
+// console.log(privousData, '==>>privousData');
+// console.log(modifyData, '=>>>modifyData')
+// console.log(modifyData.firstname, '=>>>');
 let forminform = document.querySelector('.forminform');
 Object.keys(modifyData).forEach(ele => {
-        console.log(modifyData[ele]);
+        // console.log(modifyData[ele]);
         let sumName = modifyData.firstname + " " + modifyData.lastname;
         let sumDateBir = modifyData.UserDate + modifyData.UserMonth + modifyData.UserYears;
-        console.log(sumName);
-        console.log(sumDateBir);
+        // console.log(sumName);
+        // console.log(sumDateBir);
         fullName.innerText = sumName;
         email.innerText = modifyData.UserNumber;
-        // birthday.innerHTML = `${sumDateBir}`;
+        birth1.innerHTML = modifyData.UserDate;
+        birth2.innerHTML = modifyData.UserMonth;
+        birth3.innerHTML = modifyData.UserYears;
     })
-    // forminform.innerHTML = modifyData.firstname;
-    // forminform.innerHTML = modifyData.lastname
-    // forminform.innerHTML = modifyData.UserNumber
-    // forminform.innerHTML = modifyData.UserYear
-    // forminform.innerHTML = modifyData.UserMonth
+    // post.innerText = 'hi'
+function posting(params) {
+    let div = document.createElement('div');
+    let textarea = document.createElement('textarea');
+    console.log(textarea);
+    post.appendChild(div);
+    post.appendChild(textarea)
+    div.className = 'divpost'
+    console.log(div);
+
+    // let uplodpicture = document.querySelector('image');
+    // console.log(uplodpicture)
+}
+
+// function upload(params) {
+//     let img = document.createElement('div');
+//     console.log(img)
+// }
