bf41bc504ac0e838ff64428c209451de40654a8f
Mhummad Mutahir <mutahirkareem820@gmail.com>
2023-07-02 22:26:57 +0500

    ABC

--- Dashbord/dasbord.js
+++ Dashbord/dasbord.js
@@ -12,28 +12,34 @@ let post = document.getElementById('post');
 // console.log(modifyData.firstname, '=>>>');
 let forminform = document.querySelector('.forminform');
 Object.keys(modifyData).forEach(ele => {
-        // console.log(modifyData[ele]);
-        let sumName = modifyData.firstname + " " + modifyData.lastname;
-        fullName.innerText = sumName;
-        email.innerText = modifyData.UserNumber;
-        birth1.innerHTML = modifyData.UserDate;
-        birth2.innerHTML = modifyData.UserMonth;
-        birth3.innerHTML = modifyData.UserYears;
-    })
-    // post.innerText = 'hi'
-    // function posting(params) {
-    //     let div = document.createElement('div');
-    //     let textarea = document.createElement('textarea');
-    //     console.log(textarea);
-    //     post.appendChild(div);
-    //     post.appendChild(textarea)
-    //     div.className = 'divpost'
-    //     console.log(div)
+    // console.log(modifyData[ele]);
+    let sumName = modifyData.firstname + " " + modifyData.lastname;
+    fullName.innerText = sumName;
+    email.innerText = modifyData.UserNumber;
+    birth1.innerHTML = modifyData.UserDate;
+    birth2.innerHTML = modifyData.UserMonth;
+    birth3.innerHTML = modifyData.UserYears;
+});
+
+let textarea = document.getElementById('textarea');
+let postUI = document.querySelector('.postUI');
+
+let btn_1 = document.getElementById('btn_1');
+btn_1.addEventListener('click', btn$1);
+// let newarr = [];
+// console.log(newarr);
+// let textStorage = localStorage.setItem('postValue', JSON.stringify(newarr));
+
+function btn$1() {
+    // console.log(textarea.value);
+    // let b = postUI.innerHTML = textarea.value;
+    // console.log(textarr);
+    let b = textarea.value;
+    let div1 = document.createElement('div');
+    document.body.appendChild(div1);
+    div1.innerHTML = b;
+    let setvalue = localStorage.setItem('valuepost', JSON.stringify(div1.innerHTML))
+    textarea.value = '';
 
 
-// }
-
-// function upload(params) {
-//     let img = document.createElement('div');
-//     console.log(img)
-// }
+}
