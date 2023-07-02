00faf10b653c203a57bfd09ff8c35958cac51675
Mhummad Mutahir <mutahirkareem820@gmail.com>
2023-07-03 02:16:35 +0500

    ABCDEFG

--- Dashbord/dasbord.js
+++ Dashbord/dasbord.js
@@ -1,5 +1,5 @@
 let privousData = localStorage.getItem('lOGINUSER');
-let modifyData = JSON.parse(privousData)
+let modifyData = JSON.parse(privousData);
 let fullName = document.getElementById('fullName');
 let email = document.getElementById('email');
 let birthday = document.getElementById('birthday');
@@ -7,6 +7,10 @@ let birth1 = document.querySelector('.birth1');
 let birth2 = document.querySelector('.birth2');
 let birth3 = document.querySelector('.birth3');
 let post = document.getElementById('post');
+let ui = document.getElementById('ui');
+// console.log(ui);
+
+
 // console.log(privousData, '==>>privousData');
 // console.log(modifyData, '=>>>modifyData')
 // console.log(modifyData.firstname, '=>>>');
@@ -21,25 +25,38 @@ Object.keys(modifyData).forEach(ele => {
     birth3.innerHTML = modifyData.UserYears;
 });
 
+let clock = document.getElementById('clock');
+clock.innerHTML = new Date().toLocaleString()
 let textarea = document.getElementById('textarea');
-let postUI = document.querySelector('.postUI');
+let mainpost = document.querySelector('.mainpost');
 
 let btn_1 = document.getElementById('btn_1');
 btn_1.addEventListener('click', btn$1);
-// let newarr = [];
-// console.log(newarr);
-// let textStorage = localStorage.setItem('postValue', JSON.stringify(newarr));
 
 function btn$1() {
-    // console.log(textarea.value);
-    // let b = postUI.innerHTML = textarea.value;
-    // console.log(textarr);
     let b = textarea.value;
     let div1 = document.createElement('div');
-    document.body.appendChild(div1);
-    div1.innerHTML = b;
-    let setvalue = localStorage.setItem('valuepost', JSON.stringify(div1.innerHTML))
-    textarea.value = '';
+    // mainpost.appendChild(div1);
+    mainpost.prepend(div1);
+    // <i style="position: absolute; right: 280px; top: 780px;" class="fa-solid fa-caret-down"></i>
+    div1.innerHTML = ` 
+    <div id="ui" style="position: relative;">
+    <i style=""class="fa-solid fa-caret-down"></i>
+${b}
+    </div>   <div class="like">
+    <li> <i class="fa-regular fa-heart"></i>like</li>
+    <li><i class="fa-regular fa-comment"></i>comment</li>
+    <li> <i class="fa-regular fa-share-from-square"></i>share</li>
+</div>`;
+    let divHTML = div1.innerHTML
+    let divsetvalue = localStorage.setItem('uivalues', divHTML);
+    // console.log(divsetvalue);
+
+    let privousDiv = localStorage.getItem('divHTML');
+    console.log(privousDiv);
+    // let modifyDiv = JSON.parse(privousData);
+
+
 
 
 }
